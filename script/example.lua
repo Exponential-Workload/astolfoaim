231b9d1e17749a4224a82c837182250c47fa21bd
YieldingExploiter <cc@aaathats3as.com>
2023-01-10 08:53:21 +0100

    fix: use loader

--- script/example.lua
+++ script/example.lua
@@ -1,4 +1,4 @@
 -- Example usage of the API
-local API = loadstring(game:HttpGetAsync 'http://127.0.0.1:5500/api.lua')()
+local API = getgenv().__astolfoaim_api or loadstring(game:HttpGetAsync 'https://astolfoaim.femboy.cafe/', 'apiloader')()
 API:Cleanup() -- attempt to remove whatever the last instance is | DO NOT RUN THIS AFTER ENABLING THIS INSTANCE.
 API.SetState(true, true)
