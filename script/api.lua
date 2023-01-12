3df644ad5d9a941362b141297972d328ab1e17a1
YieldingExploiter <cc@aaathats3as.com>
2023-01-12 15:34:09 +0100

    fix: mention license

--- script/api.lua
+++ script/api.lua
@@ -1,3 +1,21 @@
+--[=[
+  AstolfoAim - A Free & Open-Source Pure-Lua Roblox Aimbot Script
+  Copyright (C) 2022 YieldingExploiter
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+]=]
+
 local plrs = game:GetService 'Players'
 local lp = plrs.LocalPlayer
 local uis = game:GetService 'UserInputService'
